585da862874a4f3ebb8b93ee3b9c33bf703d3f84
howard <zhihao.zhou.bupt@gmail.com>
2025-09-08 01:37:32 +0800

    refactor codes in utils

--- agent_project_template/utils/__init__.py
+++ agent_project_template/utils/__init__.py
@@ -1,32 +1,21 @@
 """
 Utils Package
 
-Common utility functions and helpers.
+Common utility functions and helpers for Agent Project Template.
 """
 
-from .pdf_parser import parse_text_from_pdf, parse_text_from_pdf_bytes, parse_text_from_pdf_file
-from .word_parser import parse_text_from_word_bytes, parse_text_from_word_file, parse_text_from_word_bytes_xml, parse_text_from_word_bytes_mammoth
-from .logger import setup_logger, get_logger, setup_logging, setup_logfire_handler
-from .callback import post_async_callback, validate_callback_url
+# Snowflake ID utilities - fast-failing imports
 from .snowflake_generator import (
-    SnowflakeGenerator, 
-    get_snowflake_generator, 
-    generate_snowflake_id, 
+    SnowflakeGenerator,
+    get_snowflake_generator,
+    generate_snowflake_id,
     generate_snowflake_id_str
 )
-from .redis_lock import (
-    SessionLock,
-    acquire_session_lock,
-    release_session_lock,
-    session_lock_context
-)
 
-
+# Utils package exports - only utilities from this package
 __all__ = [
-    "parse_text_from_pdf", "parse_text_from_pdf_bytes", "parse_text_from_pdf_file",
-    "parse_text_from_word_bytes", "parse_text_from_word_file", "parse_text_from_word_bytes_xml", "parse_text_from_word_bytes_mammoth",
-    "setup_logger", "get_logger", "setup_logging", "setup_logfire_handler",
-    "post_async_callback", "validate_callback_url",
-    "SnowflakeGenerator", "get_snowflake_generator", "generate_snowflake_id", "generate_snowflake_id_str",
-    "SessionLock", "acquire_session_lock", "release_session_lock", "session_lock_context"
+    "SnowflakeGenerator",
+    "get_snowflake_generator",
+    "generate_snowflake_id",
+    "generate_snowflake_id_str",
 ]
